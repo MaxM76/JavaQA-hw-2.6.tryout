a79cf1aa9456e6ffd9ca63b6dc547b200541cb8c
Максим Маркелов <M.Markelov@iepi.ru>
2022-09-15 19:33:37 +0300

    Add bug1

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -25,7 +25,7 @@ public class StatisticsServiceTest {
         long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
-        long actual; service.findMax(incomesInBillions);
+        long actual=0; service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
